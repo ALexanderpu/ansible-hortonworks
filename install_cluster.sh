a7a00de0f597654216e6860479f71c34543b3e53
Alexander <pubo.alexander@gmail.com>
2018-10-10 11:59:14 -0600

    Update install_cluster.sh

--- install_cluster.sh
+++ install_cluster.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export CLOUD_TO_USE=static
+
 source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
 
 ansible-playbook -i "inventory/${cloud_to_use}" -e "cloud_name=${cloud_to_use}" playbooks/install_cluster.yml "$@"
