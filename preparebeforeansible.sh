897914683ede21b91574a5967b33650ca9114296
Alexander <pubo.alexander@gmail.com>
2018-10-11 21:14:48 -0600

    Update preparebeforeansible.sh

--- preparebeforeansible.sh
+++ preparebeforeansible.sh
@@ -1,1 +1,3 @@
-sudo apt-get install -y vim git python-pip 
+sudo apt-get install -y vim git python-pip openssh-server 
+# edit /etc/hosts with host0, host1, host2
+# ssh-keygen   ssh-copy-id bo@hosti  for i in [0,1,2]
