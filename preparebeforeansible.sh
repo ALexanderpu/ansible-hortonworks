43e44ddf9156284ba172a6a994db2a4f9e8dd0be
Alexander <pubo.alexander@gmail.com>
2018-10-11 21:17:41 -0600

    Update preparebeforeansible.sh

--- preparebeforeansible.sh
+++ preparebeforeansible.sh
@@ -1,3 +1,5 @@
 sudo apt-get install -y vim git python-pip openssh-server 
 # edit /etc/hosts with host0, host1, host2
 # ssh-keygen   ssh-copy-id bo@hosti  for i in [0,1,2]
+
+# service start order: hdfs, zookeeper, yarn, mapreduce2, hive, spark2
